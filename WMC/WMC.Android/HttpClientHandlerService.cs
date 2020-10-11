6cd7ce51b30680fbe0cee1f227551905cd08abf8
mborzyszkowski <maciej.borzyszkowski@gmail.com>
2020-10-11 20:49:07 +0200

    Add https

---
+++ WMC/WMC.Android/HttpClientHandlerService.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using WMC.Droid;
+using WMC.Services;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(HttpClientHandlerService))]
+namespace WMC.Droid
+{
+    class HttpClientHandlerService : IHttpClientHandlerService
+    {
+        public HttpClientHandler GetInsecureHandler()
+        {
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
+            {
+                if (cert.Issuer.Equals("CN=localhost"))
+                    return true;
+                return errors == System.Net.Security.SslPolicyErrors.None;
+            };
+            return handler;
+        }
+    }
+}
---
+++ WMC/WMC/Services/IHttpClientHandlerService.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http;
+
+namespace WMC.Services
+{
+    public interface IHttpClientHandlerService
+    {
+        HttpClientHandler GetInsecureHandler();
+    }
+}
